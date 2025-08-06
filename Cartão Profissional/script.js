139b803ed12c6001ad93d36edc8ab2f4a6076d58
André de Oliveira <64619562+Anferre@users.noreply.github.com>
2025-08-06 20:49:45 -0300

    Add files via upload

---
+++ Cartão Profissional/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const botao = document.getElementById("botao-iniciar");
+
+  botao.addEventListener("click", () => {
+    alert("Você clicou em Começar!");
+  });
+});
